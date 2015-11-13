c8b60e907b99c1da2cf873523b4445002ff0f47d
Andrew Dai <theman@andrewdai.co>
2015-11-13 18:54:50 -0500

    working snapshot

--- _attachments/scripts/main.js
+++ _attachments/scripts/main.js
@@ -2,19 +2,13 @@ define(["helper/util", "backbone", "models/people", "views/people_view"],
   function(util, Backbone, People, PeopleView) {
 
   console.log("CongressViz Initialized");
-  // util.get('_view/member_party?startkey=["rep"]&endkey=["rep",{}]&group=true')
-  //   .then(function(data) { console.log(data); });
-  // $('h1')[0].textContent += " holla " + d3.version;
   util.get('_view/member_summary').then(function(data) {
     console.log(data);
     var p = new People(data, {parse: true});
-    console.log(p);
-    var v = new PeopleView({collection: p});
-    console.log(v);
-    // var v1 = new PersonView({model: p1});
-    // console.log(v1);
-    // $('.people')[0].appendChild(v1.el);
-    // v1.render();
+    var v = new PeopleView({collection: p, el: '#people'});
     return p;
   });
+  // util.get('_view/enacted_without_Congressional_Tributes?startkey=[113,"s"]&endkey=[113,"s",{}]').then(function(data) {
+  //   console.log(data);
+  // });
 });
--- _attachments/scripts/views/people_view.js
+++ _attachments/scripts/views/people_view.js
@@ -1,13 +1,11 @@
 define(['underscore', 'backbone', 'views/person_view'],
 function(_, Backbone, PersonView) {
   var PeopleView = Backbone.View.extend({
-    el: '#people',
     initialize: function() {
       this.listenTo(this.collection, 'add', this.addOne);
       this.collection.each(this.addOne, this);
     },
     addOne: function(person) {
-      console.log(this.$el);
       var view = new PersonView({model: person});
       this.$el.append(view.render().el);
     }
