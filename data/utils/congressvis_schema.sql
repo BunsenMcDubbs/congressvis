31e11cdaf3217f60eb558224b78f07db4d41283a
Andrew Dai <theman@andrewdai.co>
2015-12-21 16:53:35 -0500

    change table names in mysql schema

--- data/utils/congressvis_schema.sql
+++ data/utils/congressvis_schema.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `congressvis` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `congressvis`;
 -- MySQL dump 10.13  Distrib 5.7.9, for osx10.9 (x86_64)
 --
 -- Host: localhost    Database: congressvis
@@ -16,13 +18,72 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `bill`
+-- Table structure for table `bill_sponsors`
 --
 
-DROP TABLE IF EXISTS `bill`;
+DROP TABLE IF EXISTS `bill_sponsors`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `bill` (
+CREATE TABLE `bill_sponsors` (
+  `idbill_sponsor` int(11) NOT NULL AUTO_INCREMENT,
+  `member_thomas_id` varchar(20) NOT NULL,
+  `bill_id` varchar(20) NOT NULL,
+  `sponsored_at` datetime NOT NULL,
+  `withdrawn_at` datetime DEFAULT NULL,
+  `primary_sponsor` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`idbill_sponsor`),
+  KEY `bill_id_idx` (`bill_id`),
+  KEY `member_thomas_id_idx` (`member_thomas_id`),
+  CONSTRAINT `bill_sponsor_bill_id_fk` FOREIGN KEY (`bill_id`) REFERENCES `bills` (`bill_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `thomas_id_fk` FOREIGN KEY (`member_thomas_id`) REFERENCES `members` (`thomas_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=445675 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `bill_subjects`
+--
+
+DROP TABLE IF EXISTS `bill_subjects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bill_subjects` (
+  `bill_subjects_id` int(11) NOT NULL AUTO_INCREMENT,
+  `subject_id` int(11) NOT NULL,
+  `bill_id` varchar(20) NOT NULL,
+  PRIMARY KEY (`bill_subjects_id`),
+  KEY `bill_id_fk_idx` (`bill_id`),
+  KEY `subject_id_fk_idx` (`subject_id`),
+  CONSTRAINT `bill_id_fk` FOREIGN KEY (`bill_id`) REFERENCES `bills` (`bill_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `subject_id_fk` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`subject_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=184019 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `bill_versions`
+--
+
+DROP TABLE IF EXISTS `bill_versions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bill_versions` (
+  `bill_version_id` varchar(25) NOT NULL,
+  `issued_on` datetime NOT NULL,
+  `version_code` varchar(10) NOT NULL,
+  `bill_id` varchar(20) NOT NULL,
+  PRIMARY KEY (`bill_version_id`),
+  KEY `bill_id_idx` (`bill_id`),
+  CONSTRAINT `bill_id` FOREIGN KEY (`bill_id`) REFERENCES `bills` (`bill_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `bills`
+--
+
+DROP TABLE IF EXISTS `bills`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bills` (
   `bill_id` varchar(20) NOT NULL,
   `bill_type` varchar(10) NOT NULL,
   `congress` int(11) NOT NULL,
@@ -44,79 +105,20 @@ CREATE TABLE `bill` (
   KEY `sponsor_thomas_id_fk` (`sponsor_thomas_id`),
   KEY `congress_fk_idx` (`congress`),
   KEY `subject_top_term_id_fk_idx` (`subject_top_term_id`),
-  CONSTRAINT `congress_fk` FOREIGN KEY (`congress`) REFERENCES `congress` (`congress_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `sponsor_thomas_id_fk` FOREIGN KEY (`sponsor_thomas_id`) REFERENCES `member` (`thomas_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `subject_top_term_id_fk` FOREIGN KEY (`subject_top_term_id`) REFERENCES `subject` (`subject_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `congress_fk` FOREIGN KEY (`congress`) REFERENCES `congresses` (`congress_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `sponsor_thomas_id_fk` FOREIGN KEY (`sponsor_thomas_id`) REFERENCES `members` (`thomas_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `subject_top_term_id_fk` FOREIGN KEY (`subject_top_term_id`) REFERENCES `subjects` (`subject_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `bill_sponsor`
+-- Table structure for table `congresses`
 --
 
-DROP TABLE IF EXISTS `bill_sponsor`;
+DROP TABLE IF EXISTS `congresses`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `bill_sponsor` (
-  `idbill_sponsor` int(11) NOT NULL AUTO_INCREMENT,
-  `member_thomas_id` varchar(20) NOT NULL,
-  `bill_id` varchar(20) NOT NULL,
-  `sponsored_at` datetime NOT NULL,
-  `withdrawn_at` datetime DEFAULT NULL,
-  `primary_sponsor` tinyint(1) DEFAULT '0',
-  PRIMARY KEY (`idbill_sponsor`),
-  KEY `bill_id_idx` (`bill_id`),
-  KEY `member_thomas_id_idx` (`member_thomas_id`),
-  CONSTRAINT `bill_sponsor_bill_id_fk` FOREIGN KEY (`bill_id`) REFERENCES `bill` (`bill_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `thomas_id_fk` FOREIGN KEY (`member_thomas_id`) REFERENCES `member` (`thomas_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=293069 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `bill_subject`
---
-
-DROP TABLE IF EXISTS `bill_subject`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `bill_subject` (
-  `bill_subjects_id` int(11) NOT NULL AUTO_INCREMENT,
-  `subject_id` int(11) NOT NULL,
-  `bill_id` varchar(20) NOT NULL,
-  PRIMARY KEY (`bill_subjects_id`),
-  KEY `bill_id_fk_idx` (`bill_id`),
-  KEY `subject_id_fk_idx` (`subject_id`),
-  CONSTRAINT `bill_id_fk` FOREIGN KEY (`bill_id`) REFERENCES `bill` (`bill_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `subject_id_fk` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=95946 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `bill_version`
---
-
-DROP TABLE IF EXISTS `bill_version`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `bill_version` (
-  `bill_version_id` varchar(25) NOT NULL,
-  `issued_on` datetime NOT NULL,
-  `version_code` varchar(10) NOT NULL,
-  `bill_id` varchar(20) NOT NULL,
-  PRIMARY KEY (`bill_version_id`),
-  KEY `bill_id_idx` (`bill_id`),
-  CONSTRAINT `bill_id` FOREIGN KEY (`bill_id`) REFERENCES `bill` (`bill_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `congress`
---
-
-DROP TABLE IF EXISTS `congress`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `congress` (
+CREATE TABLE `congresses` (
   `congress_id` int(11) NOT NULL,
   `start` datetime NOT NULL,
   `end` datetime NOT NULL,
@@ -127,13 +129,58 @@ CREATE TABLE `congress` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `member`
+-- Table structure for table `member_congresses`
 --
 
-DROP TABLE IF EXISTS `member`;
+DROP TABLE IF EXISTS `member_congresses`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `member` (
+CREATE TABLE `member_congresses` (
+  `idmember_congress` int(11) NOT NULL AUTO_INCREMENT,
+  `member_bioguide_id` varchar(20) NOT NULL,
+  `congress_id` int(11) NOT NULL,
+  `term_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`idmember_congress`),
+  KEY `members_by_congress_idx` (`congress_id`),
+  KEY `congress_by_member_idx` (`member_bioguide_id`),
+  KEY `member_congress_term_id_fk_idx` (`term_id`),
+  CONSTRAINT `member_congress_bioguide_id_fk` FOREIGN KEY (`member_bioguide_id`) REFERENCES `members` (`bioguide_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `member_congress_congress_id_fk` FOREIGN KEY (`congress_id`) REFERENCES `congresses` (`congress_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `member_congress_term_id_fk` FOREIGN KEY (`term_id`) REFERENCES `terms` (`term_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=118177 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `member_notes`
+--
+
+DROP TABLE IF EXISTS `member_notes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `member_notes` (
+  `member_vote_id` int(11) NOT NULL AUTO_INCREMENT,
+  `member_bioguide_id` varchar(20) NOT NULL,
+  `vote_id` varchar(20) NOT NULL,
+  `vote` enum('+','-','0') NOT NULL,
+  `party` varchar(5) DEFAULT NULL,
+  `state` varchar(2) NOT NULL,
+  `display_as` varchar(45) NOT NULL,
+  PRIMARY KEY (`member_vote_id`),
+  KEY `vote_id_idx` (`vote_id`),
+  KEY `bioguide_id_idx` (`member_bioguide_id`),
+  CONSTRAINT `bioguide_id_fk` FOREIGN KEY (`member_bioguide_id`) REFERENCES `members` (`bioguide_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `vote_id_fk` FOREIGN KEY (`vote_id`) REFERENCES `votes` (`vote_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1846164 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `members`
+--
+
+DROP TABLE IF EXISTS `members`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `members` (
   `bioguide_id` varchar(20) NOT NULL,
   `govtrack_id` varchar(20) NOT NULL,
   `thomas_id` varchar(20) DEFAULT NULL,
@@ -151,74 +198,29 @@ CREATE TABLE `member` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `member_congress`
+-- Table structure for table `subjects`
 --
 
-DROP TABLE IF EXISTS `member_congress`;
+DROP TABLE IF EXISTS `subjects`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `member_congress` (
-  `idmember_congress` int(11) NOT NULL AUTO_INCREMENT,
-  `member_bioguide_id` varchar(20) NOT NULL,
-  `congress_id` int(11) NOT NULL,
-  `term_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`idmember_congress`),
-  KEY `members_by_congress_idx` (`congress_id`),
-  KEY `congress_by_member_idx` (`member_bioguide_id`),
-  KEY `member_congress_term_id_fk_idx` (`term_id`),
-  CONSTRAINT `member_congress_bioguide_id_fk` FOREIGN KEY (`member_bioguide_id`) REFERENCES `member` (`bioguide_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `member_congress_congress_id_fk` FOREIGN KEY (`congress_id`) REFERENCES `congress` (`congress_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `member_congress_term_id_fk` FOREIGN KEY (`term_id`) REFERENCES `term` (`term_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=59131 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `member_vote`
---
-
-DROP TABLE IF EXISTS `member_vote`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `member_vote` (
-  `member_vote_id` int(11) NOT NULL AUTO_INCREMENT,
-  `member_bioguide_id` varchar(20) NOT NULL,
-  `vote_id` varchar(20) NOT NULL,
-  `vote` enum('+','-','0') NOT NULL,
-  `party` varchar(5) DEFAULT NULL,
-  `state` varchar(2) NOT NULL,
-  `display_as` varchar(45) NOT NULL,
-  PRIMARY KEY (`member_vote_id`),
-  KEY `vote_id_idx` (`vote_id`),
-  KEY `bioguide_id_idx` (`member_bioguide_id`),
-  CONSTRAINT `bioguide_id_fk` FOREIGN KEY (`member_bioguide_id`) REFERENCES `member` (`bioguide_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `vote_id_fk` FOREIGN KEY (`vote_id`) REFERENCES `vote` (`vote_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=1317590 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `subject`
---
-
-DROP TABLE IF EXISTS `subject`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `subject` (
+CREATE TABLE `subjects` (
   `subject_id` int(11) NOT NULL AUTO_INCREMENT,
   `subject` varchar(150) DEFAULT NULL,
   `subject_top_term` varchar(150) NOT NULL,
   PRIMARY KEY (`subject_id`),
   KEY `subject_top_term` (`subject_top_term`)
-) ENGINE=InnoDB AUTO_INCREMENT=1156 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2195 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `term`
+-- Table structure for table `terms`
 --
 
-DROP TABLE IF EXISTS `term`;
+DROP TABLE IF EXISTS `terms`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `term` (
+CREATE TABLE `terms` (
   `term_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `member_bioguide_id` varchar(20) NOT NULL,
   `state` char(2) NOT NULL,
@@ -235,8 +237,8 @@ CREATE TABLE `term` (
   KEY `state_district_INDEX` (`state`,`district`),
   KEY `type_party_INDEX` (`type`,`party`),
   KEY `member_bioguide_id_fk` (`member_bioguide_id`),
-  CONSTRAINT `member_bioguide_id_fk` FOREIGN KEY (`member_bioguide_id`) REFERENCES `member` (`bioguide_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=43026 DEFAULT CHARSET=utf8;
+  CONSTRAINT `member_bioguide_id_fk` FOREIGN KEY (`member_bioguide_id`) REFERENCES `members` (`bioguide_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=86007 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -248,8 +250,7 @@ CREATE TABLE `term` (
 /*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
 /*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `congressvis`.`term_AFTER_INSERT`
-AFTER INSERT ON `term`
-FOR EACH ROW
+AFTER INSERT ON `terms` FOR EACH ROW
 BEGIN
 	declare done int default false;
 	declare fetched_congress_id INT;
@@ -275,13 +276,13 @@ DELIMITER ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
--- Table structure for table `vote`
+-- Table structure for table `votes`
 --
 
-DROP TABLE IF EXISTS `vote`;
+DROP TABLE IF EXISTS `votes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `vote` (
+CREATE TABLE `votes` (
   `vote_id` varchar(20) NOT NULL,
   `bill_id` varchar(45) NOT NULL,
   `date` datetime NOT NULL,
@@ -291,7 +292,7 @@ CREATE TABLE `vote` (
   `subject` text,
   PRIMARY KEY (`vote_id`),
   KEY `vote_bill_id_idx` (`bill_id`),
-  CONSTRAINT `vote_bill_id_fk` FOREIGN KEY (`bill_id`) REFERENCES `bill` (`bill_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `vote_bill_id_fk` FOREIGN KEY (`bill_id`) REFERENCES `bills` (`bill_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -312,4 +313,4 @@ CREATE TABLE `vote` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-12-16 14:14:18
+-- Dump completed on 2015-12-21 14:27:44
