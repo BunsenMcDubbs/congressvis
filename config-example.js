fe24f94315a98fb333f1ee842cfa398fa2267e55
Andrew Dai <theman@andrewdai.co>
2015-12-21 16:55:30 -0500

    add example config file (and ignore the actual)

---
+++ config-example.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mysql: {
+    host            : 'host',
+    username        : 'username',
+    password        : 'password',
+    database        : 'congressvis',
+    connectionLimit : 100
+  }
+};
